64ac750cd350bd997ff4d1f37ec7db5674e51596
dmanslick <dvvshah@gmail.com>
2025-01-07 19:36:07 -0500

    added the poses for all of the corals

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,4 +34,55 @@ public final class Constants {
     /** Replaying from a log file. */
     REPLAY
   }
+
+  public static class CoralPose {
+    public final double x;
+    public final double y;
+    public final double elevatorPosition;
+    public final double rotationTarget;
+
+    public CoralPose(double x, double y, double elevatorPosition, double rotationTarget) {
+      this.x = x;
+      this.y = y;
+      this.elevatorPosition = elevatorPosition;
+      this.rotationTarget = rotationTarget;
+    }
+
+    public CoralPose(double x, double y) {
+      this.x = x;
+      this.y = y;
+      this.elevatorPosition = 0;
+      this.rotationTarget = 0;
+    }
+  }
+
+  public static class BlueCoralPoses {
+    public static final CoralPose p1 = new CoralPose(3.9, 5.3);
+    public static final CoralPose p2 = new CoralPose(3.676, 5);
+    public static final CoralPose p3 = new CoralPose(3.14, 4.2);
+    public static final CoralPose p4 = new CoralPose(3.15, 3.87);
+    public static final CoralPose p5 = new CoralPose(3.62, 2.933);
+    public static final CoralPose p6 = new CoralPose(3.93, 2.787);
+    public static final CoralPose p7 = new CoralPose(5.031,2.787);
+    public static final CoralPose p8 = new CoralPose(5.304,2.933);
+    public static final CoralPose p9 = new CoralPose(5.83, 3.859);
+    public static final CoralPose p10 = new CoralPose(5.83, 4.17);
+    public static final CoralPose p11 = new CoralPose(5.275, 5.127);
+    public static final CoralPose p12 = new CoralPose(3.968, 5.283);
+  }
+
+  public static class RedCoralPoses {
+    public static final CoralPose p1 = new CoralPose(13.913, 5.088);
+    public static final CoralPose p2 = new CoralPose(13.621, 5.253);
+    public static final CoralPose p3 = new CoralPose(12.47, 5.253);
+    public static final CoralPose p4 = new CoralPose(12.217, 5.127);
+    public static final CoralPose p5 = new CoralPose(11.72, 4.2);
+    public static final CoralPose p6 = new CoralPose(11.7, 3.869);
+    public static final CoralPose p7 = new CoralPose(12.236, 2.943);
+    public static final CoralPose p8 = new CoralPose(12.5, 2.806);
+    public static final CoralPose p9 = new CoralPose(13.621, 2.777);
+    public static final CoralPose p10 = new CoralPose(13.923, 2.933);
+    public static final CoralPose p11 = new CoralPose(14.39, 3.869);
+    public static final CoralPose p12 = new CoralPose(14.411, 4.171);
+  }
 }
