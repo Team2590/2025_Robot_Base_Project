d43d507f57995ff5c1bc41904c49ecc6f3a5f42b
Svadrut <svadrut@gmail.com>
2025-01-11 14:12:40 -0500

    Added PhotonAlgaeRunnable and updated PhotonCoralRunnable

--- src/main/java/frc/robot/subsystems/vision/PhotonAlgaeRunnable.java
+++ src/main/java/frc/robot/subsystems/vision/PhotonAlgaeRunnable.java
@@ -6,35 +6,35 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-public class PhotonCoralRunnable implements Runnable {
+public class PhotonAlgaeRunnable implements Runnable {
   private static NetworkTableInstance instance = NetworkTableInstance.getDefault();
-  private static PhotonCamera CoralCam = new PhotonCamera(instance, "fisheye");
+  private static PhotonCamera AlgaeCam = new PhotonCamera(instance, "fisheye");
   private static PhotonPipelineResult result;
   public static PhotonTrackedTarget target;
   private static double camHeight = VisionConstants.OBJECT_CAMERA_HEIGHT_METERS;
   private static double camPitch = VisionConstants.OBJECT_CAMERA_PITCH;
   private static double camXOffset = VisionConstants.OBJECT_CAMERA_X_DISTANCE_FROM_CENTER_METERS;
   private static double camYOffset = VisionConstants.OBJECT_CAMERA_Y_DISTANCE_FROM_CENTER_METERS;
-  private static double coralXOffset = 0;
-  private static double coralYOffset = 0;
+  private static double algaeXOffset = 0;
+  private static double algaeYOffset = 0;
 
-  public PhotonCoralRunnable() {
-    CoralCam.setPipelineIndex(1);
+  public PhotonAlgaeRunnable() {
+    AlgaeCam.setPipelineIndex(0);
   }
 
   /** Updates results on the note detection camera. */
   @Override
   public void run() {
     try {
-      result = CoralCam.getLatestResult();
+      result = AlgaeCam.getLatestResult();
       if (result.hasTargets()) {
         target = result.getBestTarget();
-        coralXOffset = camHeight / (Math.tan(camPitch - Math.toRadians(target.getPitch())));
-        coralYOffset = coralXOffset * Math.tan(Math.toRadians(target.getYaw())) + camYOffset;
+        algaeXOffset = camHeight / (Math.tan(camPitch - Math.toRadians(target.getPitch())));
+        algaeYOffset = algaeXOffset * Math.tan(Math.toRadians(target.getYaw())) + camYOffset;
       } else {
         target = null;
-        coralXOffset = 0;
-        coralYOffset = 0;
+        algaeXOffset = 0;
+        algaeYOffset = 0;
       }
     } catch (NullPointerException e) {
       return;
@@ -47,11 +47,11 @@ public class PhotonCoralRunnable implements Runnable {
    * @return y offset of note
    */
   public static double getYOffset() {
-    return coralYOffset;
+    return algaeYOffset;
   }
 
   public static double getXOffset() {
-    return coralXOffset + camXOffset;
+    return algaeXOffset + camXOffset;
   }
 
   /**
