dc0898f11c47f96cdb21c8834631cdafcdee38ab
dmanslick <dvvshah@gmail.com>
2025-01-15 19:45:26 -0500

    Added a setBlinkingMethod, added interrupt behaviors to cancel running commands, cleaned up code a little

--- src/main/java/frc/robot/subsystems/led/NemesisLED.java
+++ src/main/java/frc/robot/subsystems/led/NemesisLED.java
@@ -2,10 +2,13 @@ package frc.robot.subsystems.led;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.LEDPattern.GradientType;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -36,6 +39,16 @@ public class NemesisLED extends SubsystemBase {
         return runOnce(led::start);
     }
 
+    public Command setOff() {
+        LEDPattern.kOff.applyTo(buffer);
+        return runOnce(() -> led.setData(buffer));
+    }
+
+    public void off() {
+        LEDPattern.kOff.applyTo(buffer);
+        led.setData(buffer);
+    }
+
     // public Command rainbow() {
     //     Distance LedSpacing = Meters.of(1 / 120);
     //     LEDPattern rainbow = LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedSpacing);
@@ -47,7 +60,7 @@ public class NemesisLED extends SubsystemBase {
         Distance LedSpacing = Meters.of(1 / 120);
         LEDPattern rainbow = LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedSpacing);
         rainbow.applyTo(buffer);
-        return run(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
+        return runOnce(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
     }
 
     public Command setNemesisFlow() {
@@ -55,17 +68,32 @@ public class NemesisLED extends SubsystemBase {
         LEDPattern pattern = base.scrollAtRelativeSpeed(Percent.per(Second).of(25));
 
         pattern.applyTo(buffer);
-        return runOnce(() -> led.setData(buffer));
+        return runOnce(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
     }
 
     public Command setColor(Color color) {
         LEDPattern pattern = LEDPattern.solid(color);
         pattern.applyTo(buffer);
 
-        return runOnce(() -> led.setData(buffer));
+        return runOnce(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
     }
     
-    // public Command setBlinking(Color color, double seconds) {
+    public Command setBlinking(Color color, double seconds, double interval) {   
+        double startTime = Timer.getFPGATimestamp();
+        AtomicReference<Double> lastTimeStamp = new AtomicReference<Double>(startTime);
 
-    // }
+        return run(() -> {
+            double currentTime = Timer.getFPGATimestamp();
+            if (lastTimeStamp.get() + interval > currentTime) {
+                off();
+            } else {
+                LEDPattern.solid(color).applyTo(buffer);
+                led.setData(buffer);
+            }
+
+            lastTimeStamp.set(Timer.getFPGATimestamp());
+        })
+        .until(() -> Timer.getFPGATimestamp() >= startTime + seconds)
+        .withInterruptBehavior(InterruptionBehavior.kCancelSelf);
+    }
 }
