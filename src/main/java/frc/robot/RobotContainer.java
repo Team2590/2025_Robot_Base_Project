ece208d9031fd7f28e4677b5bfae6e2ce2748fc1
dmanslick <dvvshah@gmail.com>
2025-01-22 09:50:01 -0500

    made some changes to the leds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,8 +18,10 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
@@ -30,6 +32,7 @@ import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.led.NemesisLED;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -41,9 +44,11 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 public class RobotContainer {
   // Subsystems
   private final Drive drive;
+  private NemesisLED led = new NemesisLED(0, 100);
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
+  private CommandJoystick joystick = new CommandJoystick(1);
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
@@ -146,6 +151,9 @@ public class RobotContainer {
                             new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
                     drive)
                 .ignoringDisable(true));
+
+    joystick.button(1).onTrue(led.setColor(Color.kRed));
+    joystick.button(2).onTrue(led.setColor(Color.kBlue));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/led/NemesisLED.java
+++ src/main/java/frc/robot/subsystems/led/NemesisLED.java
@@ -2,98 +2,106 @@ package frc.robot.subsystems.led;
 
 import static edu.wpi.first.units.Units.*;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.LEDPattern.GradientType;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Dhruv Shah
  */
 public class NemesisLED extends SubsystemBase {
-    private AddressableLED led; 
-    private AddressableLEDBuffer buffer;
+  private AddressableLED led;
+  private AddressableLEDBuffer buffer;
 
-    public NemesisLED(int port, int length) {
-        led = new AddressableLED(port);
-        buffer = new AddressableLEDBuffer(length);
+  public NemesisLED(int port, int length) {
+    led = new AddressableLED(port);
+    buffer = new AddressableLEDBuffer(length);
 
-        led.setLength(length);
-        led.setData(buffer);
-        led.start();
-    }
+    led.setLength(length);
+    led.setData(buffer);
+    led.start();
+  }
 
-    public Command stop() {
-        return runOnce(led::stop);
-    }
+  public Command stop() {
+    return runOnce(led::stop);
+  }
 
-    public Command start() {
-        return runOnce(led::start);
-    }
+  public Command start() {
+    return runOnce(led::start);
+  }
 
-    public Command setOff() {
-        LEDPattern.kOff.applyTo(buffer);
-        return runOnce(() -> led.setData(buffer));
-    }
+  public Command setOff() {
+    LEDPattern.kOff.applyTo(buffer);
+    return runOnce(() -> led.setData(buffer));
+  }
 
-    public void off() {
-        LEDPattern.kOff.applyTo(buffer);
-        led.setData(buffer);
-    }
+  public void off() {
+    LEDPattern.kOff.applyTo(buffer);
+    led.setData(buffer);
+  }
 
-    // public Command rainbow() {
-    //     Distance LedSpacing = Meters.of(1 / 120);
-    //     LEDPattern rainbow = LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedSpacing);
-    //     rainbow.applyTo(buffer);
-    //     return runOnce(() -> led.setData(buffer));
-    // }
+  // public Command rainbow() {
+  //     Distance LedSpacing = Meters.of(1 / 120);
+  //     LEDPattern rainbow = LEDPattern.rainbow(255,
+  // 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedSpacing);
+  //     rainbow.applyTo(buffer);
+  //     return runOnce(() -> led.setData(buffer));
+  // }
 
-    public Command setRainbowFlow() {
-        Distance LedSpacing = Meters.of(1 / 120);
-        LEDPattern rainbow = LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedSpacing);
-        rainbow.applyTo(buffer);
-        return runOnce(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
-    }
+  public Command setRainbowFlow() {
+    Distance LedSpacing = Meters.of(1 / 120);
+    LEDPattern rainbow =
+        LEDPattern.rainbow(255, 128).scrollAtAbsoluteSpeed(MetersPerSecond.of(1), LedSpacing);
+    rainbow.applyTo(buffer);
+    return runOnce(() -> led.setData(buffer))
+        .withInterruptBehavior(InterruptionBehavior.kCancelSelf);
+  }
 
-    public Command setNemesisFlow() {
-        LEDPattern base = LEDPattern.gradient(GradientType.kDiscontinuous, Color.kWhite, Color.kBlue);
-        LEDPattern pattern = base.scrollAtRelativeSpeed(Percent.per(Second).of(25));
+  public Command setNemesisFlow() {
+    Distance ledSpacing = Meters.of(1 / 120.0);
+    LEDPattern base = LEDPattern.gradient(GradientType.kDiscontinuous, Color.kRed, Color.kWhite);
+    LEDPattern pattern = base.scrollAtRelativeSpeed(Percent.per(Second).of(25));
+    LEDPattern absolute = base.scrollAtAbsoluteSpeed(Centimeters.per(Second).of(12.5), ledSpacing);
 
-        pattern.applyTo(buffer);
-        return runOnce(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
-    }
+    pattern.applyTo(buffer);
+    led.setData(buffer);
 
-    public Command setColor(Color color) {
-        LEDPattern pattern = LEDPattern.solid(color);
-        pattern.applyTo(buffer);
+    return Commands.none();
+  }
 
-        return runOnce(() -> led.setData(buffer)).withInterruptBehavior(InterruptionBehavior.kCancelSelf);
-    }
-    
-    public Command setBlinking(Color color, double seconds, double interval) {   
-        double startTime = Timer.getFPGATimestamp();
-        AtomicReference<Double> lastTimeStamp = new AtomicReference<Double>(startTime);
+  public Command setColor(Color color) {
+    LEDPattern pattern = LEDPattern.solid(color);
+    pattern.applyTo(buffer);
 
-        return run(() -> {
-            double currentTime = Timer.getFPGATimestamp();
-            if (lastTimeStamp.get() + interval > currentTime) {
-                off();
-            } else {
-                LEDPattern.solid(color).applyTo(buffer);
-                led.setData(buffer);
-            }
+    return runOnce(() -> led.setData(buffer))
+        .withInterruptBehavior(InterruptionBehavior.kCancelSelf);
+  }
 
-            lastTimeStamp.set(Timer.getFPGATimestamp());
+  public Command setBlinking(Color color, double seconds, double interval) {
+    double startTime = Timer.getFPGATimestamp();
+    AtomicReference<Double> lastTimeStamp = new AtomicReference<Double>(startTime);
+
+    return run(() -> {
+          double currentTime = Timer.getFPGATimestamp();
+          if (lastTimeStamp.get() + interval > currentTime) {
+            off();
+          } else {
+            LEDPattern.solid(color).applyTo(buffer);
+            led.setData(buffer);
+          }
+
+          lastTimeStamp.set(Timer.getFPGATimestamp());
         })
         .until(() -> Timer.getFPGATimestamp() >= startTime + seconds)
         .withInterruptBehavior(InterruptionBehavior.kCancelSelf);
-    }
+  }
 }
