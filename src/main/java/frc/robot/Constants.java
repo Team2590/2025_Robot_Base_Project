34f0d6bdbcd6442dc51186bd40e71de399140d88
Svadrut <svadrut@gmail.com>
2025-01-22 19:56:15 -0500

    Added ArmIOTalonFX, LoggedTunableNumber, and added more constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,6 +14,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.robot.util.LoggedTunableNumber;
 
 /**
  * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
@@ -24,6 +25,25 @@ public final class Constants {
   public static final Mode simMode = Mode.SIM;
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
   public static final double loopPeriodSecs = 0.02;
+  public static final boolean tuningMode = true;
+  public static LoggedTunableNumber homeSetpoint =
+      new LoggedTunableNumber("Arm/IntakeSetpoint", .155);
+  public static final String CANBUS = "Takeover";
+
+
+  public final class ArmConstants {
+    // Fill in
+    public static final double HOME_SETPOINT = homeSetpoint.get();
+    public static final double CLIMB_SETPOINT = .198;
+    public static final double INTAKE_SETPOINT = homeSetpoint.get();
+    public static double AMP_SETPOINT = -0.27;
+    public static double TRAP_SETPOINT = -0.32;
+    public static final int ARM = 45;
+    public static final int ARM_CANCODER_ID = 44;
+    public static final double ARM_GEAR_RATIO = 266.67;
+    public static final double MAG_OFFSET = -.156;
+    public static final double ARM_MAX = -0.35; // -.3
+  }
 
   public static enum Mode {
     /** Running on a real robot. */
