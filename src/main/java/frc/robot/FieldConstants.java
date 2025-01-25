74628cf1488b3f65131193e26724d855165eeaed
Shashank Madala <madala.shashank@gmail.com>
2025-01-25 14:49:46 -0500

    updated util files

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -1,77 +1,44 @@
 package frc.robot;
-import java.awt.Polygon;
+
+import java.awt.Shape;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Path2D;
 
-import edu.wpi.first.wpilibj.DriverStation;
-
-import java.awt.Shape;
-import com.pathplanner.lib.util.GeometryUtil;
-
 public class FieldConstants {
-
-   public static enum ZONES {
-
+  public static enum ZONES {
     REEF,
     BARGE,
     PROCCESOR,
     NOTE
-    
+  }
 
+  public static double[] Reef_x = {2.16, 6.61, 2.16, 6.61};
+  public static double[] Reef_y = {6.08, 6.08, 1.57, 1.56};
+  public static Path2D ReefBounds = create_zone(Reef_x, Reef_y);
 
-   }
+  public static double[] Barge_x = {10.23, 10.23, 7.367, 7.367};
 
+  public static Path2D BargeBoundsBLUE = create_zone(Reef_x, Reef_y); // bottom
+  public static Path2D BargeBoundsRED =
+      create_zone(Reef_x, new double[] {3.5, 4.16, 3.5, .416}); // top
 
-   public static double[] Reef_x= {2.16, 6.61, 2.16,6.61} ;
-   public static double[] Reef_y= {6.08,6.08,1.57,1.56};
-   public static Path2D ReefBounds=create_zone(Reef_x, Reef_y);
+  public static Path2D create_zone(double[] x, double[] y) {
 
-
-
-   public static double[] Barge_x= {10.23 , 10.23, 7.367,7.367};
-  
-   public static Path2D BargeBoundsBLUE=create_zone(Reef_x, Reef_y); //bottom
-   public static Path2D BargeBoundsRED=create_zone(Reef_x, new double[]{3.5,4.16,3.5,.416}); //top
-
-
-
-
-   public static Path2D create_zone(double[] x, double[] y){
-    
-    Path2D bounds= new Path2D.Double();
+    Path2D bounds = new Path2D.Double();
     bounds.moveTo(x[0], y[0]);
-    
-    for(int i = 1; i < x.length; ++i) {
-        bounds.lineTo(x[i], y[i]);
+
+    for (int i = 1; i < x.length; ++i) {
+      bounds.lineTo(x[i], y[i]);
     }
 
-    
     return bounds;
+  }
 
+  public static Path2D flipZone(Path2D zone) {
+    AffineTransform t = new AffineTransform();
+    t = AffineTransform.getQuadrantRotateInstance(2, 8.785, 4.02);
+    Shape flipped = zone.createTransformedShape(t);
 
-   }
-
-   public static Path2D flipZone(Path2D zone){
-    AffineTransform t= new AffineTransform();
-    t=AffineTransform.getQuadrantRotateInstance(2,8.785,4.02);
-    Shape flipped= zone.createTransformedShape(t);
-    
     return new Path2D.Double(flipped);
-
-
-   }
-  
-   
-   
-   
-
-   
-  
-   
-  
-
-
-  
+  }
 }
-
-
---
+++ src/main/java/frc/robot/util/PolygonLocator.java
@@ -0,0 +1,43 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Constants;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+import org.littletonrobotics.junction.AutoLogOutput;
+
+public class PolygonLocator {
+
+  private QuadTree quadtree;
+
+  public PolygonLocator(List<FRCPolygon> polygons, Rectangle2D bounds) {
+    quadtree = new QuadTree(bounds);
+    for (FRCPolygon polygon : polygons) {
+      quadtree.insert(polygon, polygon.getBoundingRect());
+    }
+  }
+
+  public FRCPolygon findContainingPolygon(Translation2d robotPosition) {
+    List<FRCPolygon> candidates =
+        quadtree.query(new Rectangle2D.Double(robotPosition.getX(), robotPosition.getY(), 0, 0));
+
+    for (FRCPolygon polygon : candidates) {
+      if (polygon.contains(robotPosition)) {
+        return polygon;
+      }
+    }
+    return null;
+  }
+
+  @AutoLogOutput(key = "Odometry/zoneOfField") // this isn't freaking working
+  public String getZoneOfField(Pose2d robotPose) {
+    if (((PolygonLocator) Constants.locator).findContainingPolygon(robotPose.getTranslation())
+        != null) {
+      return ((PolygonLocator) Constants.locator)
+          .findContainingPolygon(robotPose.getTranslation())
+          .getName();
+    }
+    return "General Field";
+  }
+}
