c8aed5ab1e0ebf3a3cf082a508033605174f5f75
Svadrut <svadrut@gmail.com>
2025-01-26 14:36:01 -0500

    Changed Arm.java

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,37 +1,19 @@
 package frc.robot.subsystems.arm;
 
 import com.ctre.phoenix6.controls.DutyCycleOut;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ArmConstants;
-import frc.robot.util.HelperFn;
 import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
 public class Arm extends SubsystemBase {
   private ArmIO arm;
-  private ArmStates state;
-  private double armSetpoint;
-  private double tolerance = .005;
-  private DutyCycleOut power = new DutyCycleOut(0);
-  private LoggedTunableNumber homeSetpoint = new LoggedTunableNumber("Arm/homeSetpoint", .168);
-  private double targetHome = ArmConstants.HOME_SETPOINT;
-  private boolean isClimbing = false;
 
   private final ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
 
   public Arm(ArmIO io) {
-    state = ArmStates.STOPPED;
     arm = io;
-  }
-
-  public static enum ArmStates {
-    STOPPED,
-    MANUAL,
-    AT_SETPOINT,
-    APPROACHINGSETPOINT,
-    REEF, /*STOWED, */
-    APPROACHING_HOME,
-    HOME,
   }
 
   @Override
@@ -39,110 +21,23 @@ public class Arm extends SubsystemBase {
     arm.updateTunableNumbers();
     arm.updateInputs(inputs);
     Logger.processInputs("Arm", inputs);
-    switch (state) {
-      case STOPPED:
-        arm.stop();
-        break;
-      case MANUAL:
-        arm.setPower(power);
-        break;
-      case APPROACHINGSETPOINT:
-        arm.setPosition(armSetpoint);
-        if (!isArmAtSetPointPosition(armSetpoint)) {
-          state = ArmStates.APPROACHINGSETPOINT;
-        } else {
-          state = ArmStates.AT_SETPOINT;
-        }
-        break;
-      case AT_SETPOINT:
-        if (isArmAtSetPointPosition(armSetpoint)) {
-          state = ArmStates.AT_SETPOINT;
-          if (!isClimbing && armSetpoint == ArmConstants.HOME_SETPOINT) {
-            state = ArmStates.HOME;
-          }
-        } else {
-          state = ArmStates.APPROACHINGSETPOINT;
-        }
-        break;
-      case REEF:
-        break;
-      case APPROACHING_HOME:
-        arm.setPosition(ArmConstants.HOME_SETPOINT);
-        if (isArmAtSetPointPosition(ArmConstants.HOME_SETPOINT)) {
-          state = ArmStates.HOME;
-        }
-      case HOME:
-        if (!isArmAtSetPointPosition(ArmConstants.HOME_SETPOINT)) {
-          state = ArmStates.APPROACHING_HOME;
-        }
-        break;
-    }
-  }
-
-  private boolean isArmAtSetPointPosition(double setPoint) {
-    return HelperFn.isWithinTolerance(inputs.armabspos, setPoint, tolerance);
   }
 
   /** Run open loop at the specified voltage. */
-  public void setPosition(double setpoint) {
-    if (setpoint <= ArmConstants.ARM_MAX) {
-      setpoint = ArmConstants.ARM_MAX;
-    } else if (setpoint >= ArmConstants.CLIMB_SETPOINT) {
-      setpoint = ArmConstants.CLIMB_SETPOINT;
-    }
-    armSetpoint = setpoint;
-    if (!HelperFn.isWithinTolerance(inputs.armabspos, armSetpoint, tolerance)) {
-      state = ArmStates.APPROACHINGSETPOINT;
-    } else {
-      state = ArmStates.AT_SETPOINT;
-    }
+  public Command setPosition(double setpoint) {
+    return runOnce(() -> arm.setPosition(setpoint));
   }
 
-  // public void setManualPower(double percent) {
-  //   manualPower = percent;
-  // }
-
-  public void setHome() {
-    if (isArmAtSetPointPosition(ArmConstants.HOME_SETPOINT)) {
-      state = ArmStates.HOME;
-    } else {
-      state = ArmStates.APPROACHING_HOME;
-    }
+  public Command resetarm() {
+    return runOnce(arm::resetArm);
   }
 
-  public void setClimb() {
-    isClimbing = true;
-    if (!isArmAtSetPointPosition(ArmConstants.CLIMB_SETPOINT)) {
-      setPosition(ArmConstants.CLIMB_SETPOINT);
-    }
-  }
-
-  public void resetarm() {
-    isClimbing = false;
-    arm.resetArm();
-  }
-
-  public void manual(DutyCycleOut request) {
-    power = request;
-    state = ArmStates.MANUAL;
-  }
-
-  public void armmanualdown() {
-    state = ArmStates.MANUAL;
-    DutyCycleOut power = new DutyCycleOut(0.1);
-    arm.setPower(power);
+  public Command manual(DutyCycleOut request) {
+    return runOnce(() -> arm.setPower(request));
   }
 
   /** Stops the flywheel. */
-  public void setStopped() {
-    state = ArmStates.STOPPED;
-  }
-
-  public double getAbsolutePosition() {
-    return inputs.armabspos;
-  }
-
-  public ArmStates getState() {
-    return state;
+  public Command stop() {
+    return runOnce(arm::stop);
   }
 }
