d15b6a85405f179aeef1e4a33783575b67e45c36
Anthony Heulitt <anthonyh1234@gmail.com>
2025-01-26 20:09:51 -0500

    reverted Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -64,8 +64,8 @@ public class Robot extends LoggedRobot {
         break;
       case LARRY:
         // Running on a real robot, log to a USB stick ("/U/logs")
-        // Logger.addDataReceiver(new WPILOGWriter());
-        // Logger.addDataReceiver(new NT4Publisher());
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
         break;
       case SIM:
         // Running a physics simulator, log to NT
