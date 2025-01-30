656bb46b7e92b863376d13e059e00a7e4dc99293
dmanslick <dvvshah@gmail.com>
2025-01-29 19:03:00 -0500

    Explitly set g type for the elevator

---
+++ src/main/java/frc/robot/util/NemesisMathUtil.java
@@ -0,0 +1,7 @@
+package frc.robot.util;
+
+public class NemesisMathUtil {
+    public static boolean isApprox(Number value, Number tolerance, Number desired) {
+        return value.doubleValue() > desired.doubleValue() - tolerance.doubleValue() && value.doubleValue() < desired.doubleValue() + tolerance.doubleValue();
+    }
+}
