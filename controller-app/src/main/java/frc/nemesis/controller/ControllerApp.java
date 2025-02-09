ed3f1f8b2b49912d48879e36ed8c517951ecc92b
dmanslick <dvvshah@gmail.com>
2025-02-09 10:47:33 -0500

    not sure

--- controller-app/src/main/java/frc/nemesis/controller/ControllerApp.java
+++ controller-app/src/main/java/frc/nemesis/controller/ControllerApp.java
@@ -3,17 +3,10 @@ package frc.nemesis.controller;
 import java.util.HashMap;
 import java.util.Map;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
