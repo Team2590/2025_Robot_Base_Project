079134f5ef764dfb88d979ebf169ca9d8ceb35bb
peter-wolfe <wolfe@pobox.com>
2025-02-10 21:14:08 -0500

    increased kP value for the kronos drive motor

--- src/main/java/frc/robot/generated/TunerConstantsKronos.java
+++ src/main/java/frc/robot/generated/TunerConstantsKronos.java
@@ -36,7 +36,7 @@ public class TunerConstantsKronos {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(0.1).withKI(0).withKD(0).withKS(0.16917).withKV(0.12560);
+      new Slot0Configs().withKP(0.155).withKI(0).withKD(0).withKS(0.16917).withKV(0.12560);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
