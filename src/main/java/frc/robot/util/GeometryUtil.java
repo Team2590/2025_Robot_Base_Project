0b7b4c7298f4135231ccb66c56386e18b1e097d0
Svadrut <svadrut@gmail.com>
2025-02-10 20:10:03 -0500

    Made GeometryUtil and distanceBetweenPoses

---
+++ src/main/java/frc/robot/util/GeometryUtil.java
@@ -0,0 +1,9 @@
+package frc.robot.util;
+
+import edu.wpi.first.math.geometry.Pose2d;
+
+public class GeometryUtil {
+    public static double distanceBetweenPoses(Pose2d pose1, Pose2d pose2) {
+        return Math.hypot(pose2.getX() - pose1.getX(), pose2.getY() - pose1.getY());
+    }
+}
