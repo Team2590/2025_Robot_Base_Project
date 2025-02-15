60471ececabdeb0396cbf4ab1144e030cae88230
codejvn <65090247+codejvn@users.noreply.github.com>
2025-02-15 14:14:17 -0500

    added NemesisTImedCommand implementation into scoring factory + comments

--- src/main/java/frc/robot/command_factories/ScoringFactory.java
+++ src/main/java/frc/robot/command_factories/ScoringFactory.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
+import frc.robot.util.NemesisTimedCommand;
 
 /**
  * Factory class for creating complex scoring-related commands.
@@ -22,7 +23,7 @@ public class ScoringFactory {
     return new SequentialCommandGroup(
             ElevatorFactory.setPositionBlocking(Constants.ElevatorConstantsLeonidas.ELEVATOR_L4_POS),
             ArmFactory.setPositionBlocking(Constants.ArmConstantsLeonidas.ARM_L4_POS),
-            EndEffectorFactory.runEndEffectorOuttake())
+            NemesisTimedCommand.generateTimedCommand(EndEffectorFactory.runEndEffectorOuttake(), 1))
         .withName("Score L4");
   }
 
@@ -36,7 +37,7 @@ public class ScoringFactory {
     return new SequentialCommandGroup(
             ElevatorFactory.setPositionBlocking(Constants.ElevatorConstantsLeonidas.ELEVATOR_L3_POS),
             ArmFactory.setPositionBlocking(Constants.ArmConstantsLeonidas.ARM_L3_POS),
-            EndEffectorFactory.runEndEffectorOuttake())
+            NemesisTimedCommand.generateTimedCommand(EndEffectorFactory.runEndEffectorOuttake(), 1))
         .withName("Score L3");
   }
 
@@ -50,7 +51,7 @@ public class ScoringFactory {
     return new SequentialCommandGroup(
             ElevatorFactory.setPositionBlocking(Constants.ElevatorConstantsLeonidas.ELEVATOR_L2_POS),
             ArmFactory.setPositionBlocking(Constants.ArmConstantsLeonidas.ARM_L2_POS),
-            EndEffectorFactory.runEndEffectorOuttake())
+            NemesisTimedCommand.generateTimedCommand(EndEffectorFactory.runEndEffectorOuttake(), 1))
         .withName("Score L2");
   }
 
@@ -63,8 +64,8 @@ public class ScoringFactory {
   public static Command scoreL1() {
     return new SequentialCommandGroup(
             IntakeFactory.setHomePosition(),
-            IntakeFactory.runIntake(() -> Constants.IntakeConstantsLeonidas.INTAKE_CORAL_OUTTAKE_SPEED))
-        .withName("Score L1");
+            NemesisTimedCommand.generateTimedCommand(IntakeFactory.runIntake(() -> Constants.IntakeConstantsLeonidas.INTAKE_CORAL_OUTTAKE_SPEED), 1)
+        .withName("Score L1"));
   }
 
   /**
--- src/main/java/frc/robot/util/NemesisTimedCommand.java
+++ src/main/java/frc/robot/util/NemesisTimedCommand.java
@@ -8,9 +8,9 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 public class NemesisTimedCommand{
     /**
      * Generates a command to run for a certain amount of time
-     * @param c
-     * @param time
-     * @return
+     * @param c - The command to be run
+     * @param time - time (seconds) to run the command for 
+     * @return a ParallelRaceGroup with the given command and a WaitCommand of the given time
      */
     public static Command generateTimedCommand(Command c, double time){
         return new ParallelRaceGroup(c, new WaitCommand(time)); 
