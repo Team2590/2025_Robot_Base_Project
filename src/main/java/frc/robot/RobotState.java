c0d01a3552f590ff6b2cc5eecca7017138774056
dmanslick <dvvshah@gmail.com>
2025-02-15 09:57:59 -0500

    Created RobotState.java

---
+++ src/main/java/frc/robot/RobotState.java
@@ -0,0 +1,29 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class RobotState extends SubsystemBase {
+    public RobotState() {
+        // TODO: initalize instance variables and objects
+    }
+
+    @Override
+    public void periodic() {
+        // TODO: check various states
+    }
+
+    public boolean hasCoralIntake() {
+        // TODO: add logic based on intake
+        return false;
+    }
+
+    public boolean hasCoralEndEffector() {
+        // TODO: add logic based on intake
+        return false;
+    }
+
+    public boolean hasCoralAlgae() {
+        // TODO: add logic based on intake
+        return false;
+    }
+}
