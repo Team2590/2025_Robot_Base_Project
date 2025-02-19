ef03e27db1644ee1360d9119922bd0de846647dc
Svadrut <svadrut@gmail.com>
2025-02-19 17:49:24 -0500

    Added Climb ClimbIO and ClimbIOTalonFX

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -55,7 +55,6 @@ import frc.robot.subsystems.intake.IntakeIOTalonFX;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIOPhotonVision;
 import frc.robot.subsystems.vision.VisionIOPhotonVision.CameraConfig;
-import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
 import java.util.List;
 import lombok.Getter;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
---
+++ src/main/java/frc/robot/subsystems/climb/ClimbIO.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.climb;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ClimbIO {
+  @AutoLog
+  class ClimbIOInputs {
+    public boolean connected = false;
+    public double positionRads = 0.0;
+    public double velocityRadsPerSec = 0.0;
+    public double appliedVoltage = 0.0;
+    public double supplyCurrentAmps = 0.0;
+    public double torqueCurrentAmps = 0.0;
+    public double tempCelsius = 0.0;
+    public double rotationCount = 0.0;
+    public double statorCurrentAmps = 0.0;
+  }
+
+  public void updateInputs(ClimbIOInputs inputs);
+
+  public void setVoltage(double voltage);
+
+  public void setVelocity(double speed);
+
+  public void stop();
+}
