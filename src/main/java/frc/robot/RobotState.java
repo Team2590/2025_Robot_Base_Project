de666cfe8077af1f68b5889819c2f71b315286b4
yolin167 <prahasai28@gmail.com>
2025-02-20 20:24:37 -0500

    Implemented Mr.Fazal's skeleton code and added documentation for robotstate class

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -5,8 +5,12 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.endeffector.EndEffector;
 import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.vision.Vision;
 import frc.robot.util.PolygonLocator;
 import frc.robot.FieldConstants;
 
@@ -14,13 +18,58 @@ public class RobotState extends SubsystemBase{
     Alliance alliance;
     Pose2d robotPose;
     private String currentZone;
-    private EndEffector endEffector = RobotContainer.getEndEffector();
-    private Intake intake = RobotContainer.getIntake();
-    
+    private final Arm arm;
+    private final Drive drive;
+    private final Elevator elevator;
+    private final Vision vision;
+    private final EndEffector endEffector;
+    private final Intake intake;
+    private static RobotState instance;
 
+
+    private RobotState(
+      Arm arm,
+      Drive drive,
+      Elevator elevator,
+      EndEffector endEffector,
+      Intake intake,
+      Vision vision) {
+    this.arm = arm;
+    this.drive = drive;
+    this.elevator = elevator;
+    this.endEffector = endEffector;
+    this.intake = intake;
+    this.vision = vision;
+  }
+    
+  public static RobotState initialize(
+    Arm arm,
+    Drive drive,
+    Elevator elevator,
+    EndEffector endEffector,
+    Intake intake,
+    Vision vision) {
+        if (instance != null) {
+            throw new IllegalStateException("RobotState has already been initialized");
+        }
+        instance = new RobotState(arm, drive, elevator, endEffector, intake, vision);
+        return instance;
+    }
+
+    public static RobotState getInstance() {
+        if (instance == null) {
+          throw new IllegalStateException("RobotState has not been initialized");
+        }
+        return instance;
+    }
+
+    public Pose2d getPose() {
+        return drive.getPose();
+    }  
+  
     public void periodic() {
         alliance = DriverStation.getAlliance().get();
-        robotPose = RobotContainer.getDrive().getPose();
+        robotPose = drive.getPose();
         currentZone = Constants.locator.getZoneOfField(robotPose);
     }
 
