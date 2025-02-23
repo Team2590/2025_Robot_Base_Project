eaa323a6bd9801c2b64b9ce3c6581fa216fc9e85
yolin167 <prahasai28@gmail.com>
2025-02-22 21:13:32 -0500

    Added stow intake after intaking coral and algae
    Co-authored-by: Svadrut <svadrut@gmail.com>

--- src/main/java/frc/robot/command_factories/GamePieceFactory.java
+++ src/main/java/frc/robot/command_factories/GamePieceFactory.java
@@ -2,6 +2,7 @@ package frc.robot.command_factories;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 
 public class GamePieceFactory {
@@ -12,14 +13,18 @@ public class GamePieceFactory {
   }
 
   public static Command intakeAlgaeGround() {
-    return new ParallelCommandGroup(
-        IntakeFactory.setIntakeAlgaePosition(),
-        IntakeFactory.runIntake(() -> Constants.IntakeConstantsLeonidas.INTAKE_ALGAE_INTAKE_SPEED));
+    return new SequentialCommandGroup(
+        new ParallelCommandGroup(
+            IntakeFactory.setIntakeAlgaePosition(),
+            IntakeFactory.runIntake(() -> Constants.IntakeConstantsLeonidas.INTAKE_ALGAE_INTAKE_SPEED)),
+        IntakeFactory.setHoldingAlgaePosition());
   }
 
   public static Command intakeCoralGround() {
-    return new ParallelCommandGroup(
-        IntakeFactory.setIntakeCoralPosition(),
-        IntakeFactory.runIntake(() -> Constants.IntakeConstantsLeonidas.INTAKE_CORAL_INTAKE_SPEED));
+    return new SequentialCommandGroup(
+        new ParallelCommandGroup(
+            IntakeFactory.setIntakeCoralPosition(),
+            IntakeFactory.runIntake(() -> Constants.IntakeConstantsLeonidas.INTAKE_CORAL_INTAKE_SPEED)),
+        IntakeFactory.setHoldingAlgaePosition());
   }
 }
