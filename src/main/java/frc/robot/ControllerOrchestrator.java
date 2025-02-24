77ec8a56b6cc6eadb9b26485b8f2dfb9836e3b59
aheulitt <94087314+aheulitt@users.noreply.github.com>
2025-02-23 20:04:38 -0500

    Master controller app sim testing (#104)
    * Elevator and intake commands
    
    * moved arbitary elevator setpoints from map to constants, fixed delimitter for grabbing scoring level
    
    * Added a field image. Need to double check source poses - check if blue or red alliance
    
    * Added resources folder to grab field image
    
    * Added alliance logic
    
    * updated elevator constants from master, alliance logic
    
    * whitespace and vendor lib
    
    * add in vendor library
    
    * implemented try/catch
    
    * Added loggers, ArmSetpoint, try catch exceptions
    
    * testing
    
    * added a command method for getElevatorSetpoint for controller app, added in bindings in robot container as well
    
    * putting the safety checking in
    
    * got rid of lingering merge conflict
    
    * updating setpoints
    
    ---------
    
    Co-authored-by: Aayan Fazal <115327839+AayanFazal@users.noreply.github.com>
    Co-authored-by: peter-wolfe <wolfe@pobox.com>

--- src/main/java/frc/robot/ControllerOrchestrator.java
+++ src/main/java/frc/robot/ControllerOrchestrator.java
@@ -6,17 +6,37 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.elevator.Elevator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
+import org.littletonrobotics.junction.Logger;
 
 public class ControllerOrchestrator extends SubsystemBase {
   private HashMap<String, Pose2d> poseMap = new HashMap<String, Pose2d>();
   private HashMap<String, Double> elevatorSetpointMap = new HashMap<String, Double>();
-  private HashMap<String, Double> endeffectorWristSetpointMap = new HashMap<String, Double>();
+  private HashMap<String, Double> armSetpointMap = new HashMap<String, Double>();
+  private String alliance;
 
   public ControllerOrchestrator() {
-    // Optional<Alliance> ally = DriverStation.getAlliance();
-    String alliance = "Red";
+    Optional<Alliance> ally = DriverStation.getAlliance();
+    if (ally.isPresent()) {
+      if (ally.get() == Alliance.Red) {
+        alliance = "Red";
+      }
+      if (ally.get() == Alliance.Blue) {
+        alliance = "Blue";
+      }
+    } else {
+      // SIM mode
+      alliance = "Red";
+    }
     switch (alliance) {
       case "Red":
         poseMap.put("Sright", FieldConstants.RedReefPoses.Sright);
@@ -31,7 +51,12 @@ public class ControllerOrchestrator extends SubsystemBase {
         poseMap.put("NEleft", FieldConstants.RedReefPoses.NEleft);
         poseMap.put("SEright", FieldConstants.RedReefPoses.SEright);
         poseMap.put("SEleft", FieldConstants.RedReefPoses.SEleft);
+
+        // Test
+        poseMap.put("sourceTop", FieldConstants.CoralStationRight);
+        poseMap.put("sourceBottom", FieldConstants.CoralStationLeft);
         break;
+
       case "Blue":
         poseMap.put("Sright", FieldConstants.BlueReefPoses.Sright);
         poseMap.put("Sleft", FieldConstants.BlueReefPoses.Sleft);
@@ -49,18 +74,19 @@ public class ControllerOrchestrator extends SubsystemBase {
     }
 
     // TODO: add real values
-    elevatorSetpointMap.put("station", 0.0);
-    elevatorSetpointMap.put("L1", 1.0);
-    elevatorSetpointMap.put("L2", 2.0);
-    elevatorSetpointMap.put("L3", 3.0);
-    elevatorSetpointMap.put("L4", 4.0);
+    elevatorSetpointMap.put("source", Constants.ElevatorConstantsLeonidas.ELEVATOR_SOURCE_POS);
+    elevatorSetpointMap.put(
+        "L1", Constants.ElevatorConstantsLeonidas.ELEVATOR_SOURCE_POS); // need to find
+    elevatorSetpointMap.put("L2", Constants.ElevatorConstantsLeonidas.ELEVATOR_L2_POS);
+    elevatorSetpointMap.put("L3", Constants.ElevatorConstantsLeonidas.ELEVATOR_L3_POS);
+    elevatorSetpointMap.put("L4", Constants.ElevatorConstantsLeonidas.ELEVATOR_L4_POS);
 
     // TODO: add real values
-    endeffectorWristSetpointMap.put("station", 0.0);
-    endeffectorWristSetpointMap.put("L1", 1.0);
-    endeffectorWristSetpointMap.put("L2", 2.0);
-    endeffectorWristSetpointMap.put("L3", 3.0);
-    endeffectorWristSetpointMap.put("L4", 4.0);
+    armSetpointMap.put("source", Constants.ArmConstantsLeonidas.ARM_INTAKE_SOURCE_POSITION);
+    armSetpointMap.put("L1", Constants.ArmConstantsLeonidas.ARM_SCORING_CORAL_POS);
+    armSetpointMap.put("L2", Constants.ArmConstantsLeonidas.ARM_SCORING_CORAL_POS);
+    armSetpointMap.put("L3", Constants.ArmConstantsLeonidas.ARM_SCORING_CORAL_POS);
+    armSetpointMap.put("L4", Constants.ArmConstantsLeonidas.ARM_SCORING_CORAL_POS_L4);
   }
 
   public NetworkTableEntry getTableEntry(String key) {
@@ -86,12 +112,36 @@ public class ControllerOrchestrator extends SubsystemBase {
   }
 
   public double getElevatorSetpoint() {
-    String elevatorSetpointKey = getValue("moveTo").split("_")[0];
+    String elevatorSetpointKey;
+    try {
+      elevatorSetpointKey = getValue("moveTo").split("_")[1];
+    } catch (Exception e) {
+      elevatorSetpointKey = "source";
+      System.out.println("E STACK " + e);
+    }
+    Logger.recordOutput("ControllerApp/ElevatorSetpoint", elevatorSetpointKey);
     return elevatorSetpointMap.get(elevatorSetpointKey);
   }
 
-  public double endeffectorWristSetpoint() {
-    String endeffectorWristSetpointKey = getValue("moveTo").split("_")[1];
-    return endeffectorWristSetpointMap.get(endeffectorWristSetpointKey);
+  public Command getElevatorSetpoint(Elevator elevator) {
+    HashSet<Subsystem> requirements = new HashSet<>();
+    requirements.add(elevator);
+    return Commands.defer(
+        () -> {
+          Double elevatorSetpoint = getElevatorSetpoint();
+          return elevator.setPosition(elevatorSetpoint);
+        },
+        requirements);
+  }
+
+  public double getArmSetpoint() {
+    String armSetpointKey;
+    try {
+      armSetpointKey = getValue("moveTo").split("_")[1];
+    } catch (Exception e) {
+      armSetpointKey = "source";
+    }
+    Logger.recordOutput("ControllerApp/ArmSetpoint", armSetpointKey);
+    return armSetpointMap.get(armSetpointKey);
   }
 }
