15618e9b7d4674adf043438d489ace0c617baf1c
Jeevan Sailesh <65090247+codejvn@users.noreply.github.com>
2025-02-24 19:46:51 -0500

    updating constants  for end effector (#112)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -334,7 +334,7 @@ public final class Constants {
     public static final boolean brake = true; // TODO
     public static final double reduction = 1; // TODO
     public static final int proxSensor_ID = 0; // TODO
-    public static final double INTAKE_VOLTAGE = 3;
+    public static final double INTAKE_VOLTAGE = 2;
     public static final double EJECT_VOLTAGE = -INTAKE_VOLTAGE;
   }
 
