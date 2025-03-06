816ab4b2f454e74782d0e01e245469f3a31408cc
dmanslick <dvvshah@gmail.com>
2025-03-05 20:09:15 -0500

    starting working on default commands

---
+++ src/main/java/frc/robot/commands/ElevatorDefaultCommand.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.elevator.Elevator;
+
+public class ElevatorDefaultCommand extends Command {
+    public ElevatorDefaultCommand(Elevator elevator) {
+        
+    }
+}
