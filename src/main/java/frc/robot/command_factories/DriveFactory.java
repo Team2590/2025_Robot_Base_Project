8fd385a0ab895687881b5d79b986ea3fbb606067
Stephen David <sdavid14@gmail.com>
2025-03-17 21:52:35 -0400

    proof-of-concept only - needs cleanup and integration.

--- src/main/java/frc/robot/command_factories/DriveFactory.java
+++ src/main/java/frc/robot/command_factories/DriveFactory.java
@@ -1,9 +1,12 @@
 package frc.robot.command_factories;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
 import frc.robot.commands.DriveCommands;
+import java.util.function.Supplier;
 
 /**
  * Factory class for creating commands related to the drive subsystem.
@@ -38,5 +41,32 @@ public class DriveFactory {
 
     // RobotContainer.factoryCommands.add("Drive To Pose");
     return DriveCommands.driveToPose(targetPose).withName("Drive To Pose");
+  }
+
+  /**
+   * Creates a command that first uses path following to get close to the target, then uses PID
+   * control for final precise alignment.
+   */
+  public static Command hybridPreciseAlignment(
+      Supplier<Pose2d> targetPoseSupplier, Rotation2d approachDirection) {
+
+    return Commands.sequence(
+            DriveCommands.preciseAlignment(
+                RobotContainer.getDrive(), targetPoseSupplier, approachDirection),
+            DriveCommands.pidAlignment(RobotContainer.getDrive(), targetPoseSupplier))
+        .withName("HybridPreciseAlignment");
+  }
+
+  /**
+   * Creates a command for testing the hybrid precise alignment in simulation.
+   *
+   * @param targetPoseSupplier The supplier for the target pose
+   * @param approachDirection The direction to approach the target from
+   * @return A command that performs precise alignment using both methods
+   */
+  public static Command hybridPreciseAlignmentSim(
+      Supplier<Pose2d> targetPoseSupplier, Rotation2d approachDirection) {
+
+    return hybridPreciseAlignment(targetPoseSupplier, approachDirection);
   }
 }
