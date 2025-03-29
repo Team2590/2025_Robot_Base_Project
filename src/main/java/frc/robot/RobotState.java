22b179c28e8ea8af1f4c489ae19ba265d147c553
Asim Fazal <fazal.asim@gmail.com>
2025-03-29 15:56:11 -0400

    source formatting

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -212,7 +212,7 @@ public class RobotState extends SubsystemBase {
         offset = Constants.ArmConstantsLeonidas.BACK_HORIZONTAL;
         magnitude = -1;
       }
-      coralScoringSetpoints.armSetpoint =  magnitude * coralScoringSetpoints.armSetpoint + offset;
+      coralScoringSetpoints.armSetpoint = magnitude * coralScoringSetpoints.armSetpoint + offset;
       coralScoringSetpoints.armPlaceSetpoint =
           magnitude * coralScoringSetpoints.armPlaceSetpoint + offset;
 
