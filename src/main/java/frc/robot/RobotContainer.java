be48340dbbac31dabafb65051f2626e281d4b4a7
Jeevan Sailesh <65090247+codejvn@users.noreply.github.com>
2025-03-31 16:28:36 -0400

    Updated driver buttons (#242)
    * climb and some aligning things
    
    * added pov buttons and spit button
    
    * Revert "climb and some aligning things"
    
    This reverts commit db047033b292844f4c3806d9e40a691681883183.
    
    ---------
    
    Co-authored-by: peter-wolfe <wolfe@pobox.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -486,9 +486,10 @@ public class RobotContainer {
     leftJoystick.povRight().whileTrue(ScoringFactory.score(Level.L2));
     leftJoystick.povDown().whileTrue(ScoringFactory.score(Level.L3));
     leftJoystick.povLeft().whileTrue(ScoringFactory.score(Level.L4));
-    rightJoystick.povDown().whileTrue(ScoringFactory.score(Level.L1));
+    // rightJoystick.povDown().whileTrue(ScoringFactory.score(Level.L1));
     leftJoystick.button(2).whileTrue(ScoringFactory.stow());
     rightJoystick.button(4).and(leftJoystick.trigger()).whileTrue(ScoringFactory.scoreProcessor());
+    //change to follow thru 
     leftJoystick
         .trigger()
         .and(rightJoystick.button(4).negate())
@@ -510,6 +511,11 @@ public class RobotContainer {
         .and(rightJoystick.button(4).negate())
         .whileTrue(GamePieceFactory.intakeCoralGroundAndHandoff());
 
+        rightJoystick
+        .trigger()
+        .and(rightJoystick.button(4))
+        .whileTrue(GamePieceFactory.intakeAlgaeGround());
+
     rightJoystick.povUp().whileTrue(EndEffectorFactory.runEndEffectorManual());
 
     // Manual Elevator Control
@@ -528,6 +534,12 @@ public class RobotContainer {
                 .ignoringDisable(true));
     rightJoystick.button(8).onTrue(elevator.resetRotationCountCommand());
 
+    // LEFT POV UP SCORE BARGE
+    leftJoystick.povUp().whileTrue(ScoringFactory.scoreAlgaeBarge());
+    // RIGHT POV DOWN OUTTAKE ITNAKE (SPIT)
+    rightJoystick.povDown().whileTrue(IntakeFactory.runIntakeVoltage(() -> Constants.IntakeConstantsLeonidas.INTAKE_CORAL_OUTTAKE_SPEED));
+
+
     /* for tuning setpoints */
     // rightJoystick
     //     .trigger()
@@ -536,6 +548,7 @@ public class RobotContainer {
     //         Atlas.synchronize(
     //             intake.getArmTunableNumber(), elevator.getTunableNumber(),
     // arm.getTunableNumber()));
+
   }
 
   private void configureButtonBindingsTuning() {
