39e74ac043a399625fe99e29b15c5f2a24a80263
Anthony Heulitt <anthonyh1234@gmail.com>
2025-03-31 23:11:06 -0400

    added a 1.5 in offset for back scoring

--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -224,6 +224,10 @@ public class RobotState extends SubsystemBase {
 
     targetPose = drive.flipScoringSide(originalTargetPose.get());
 
+    if (aligningState.get() == AligningState.ALIGNING_BACK) {
+      targetPose = FieldConstants.convertBackScoring(targetPose);
+    }
+
     Logger.recordOutput("RobotState/Pose", targetPose);
     Logger.recordOutput("RobotState/CoralArmSetpoint", coralScoringSetpoints.armSetpoint);
     Logger.recordOutput("RobotState/CoralArmPlaceSetpoint", coralScoringSetpoints.armPlaceSetpoint);
