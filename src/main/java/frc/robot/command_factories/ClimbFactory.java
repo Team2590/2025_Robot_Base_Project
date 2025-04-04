c23ed88e51c72d2863f8decb3d71f4c74b3c9172
Anthony Heulitt <anthonyh1234@gmail.com>
2025-04-03 22:08:10 -0400

    reduce manual climb voltage

--- src/main/java/frc/robot/command_factories/ClimbFactory.java
+++ src/main/java/frc/robot/command_factories/ClimbFactory.java
@@ -14,7 +14,7 @@ public class ClimbFactory {
 
   public static Command manualRunClimb(){
 
-    return RobotContainer.getClimb().runClimb(Constants.ClimbConstantsLeonidas.CLIMB_VOLTAGE-3);
+    return RobotContainer.getClimb().runClimb(Constants.ClimbConstantsLeonidas.CLIMB_VOLTAGE-6);
   }
 }
 
